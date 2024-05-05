4907cee0c179d6acfef4b11bd612a38c6559eabf
batch23b <bpcsekharreddy@yahoo.com>
2024-05-05 23:16:39 +0530

    upload

---
+++ switch.sh
@@ -0,0 +1,16 @@
+echo "enter activity input:"
+read key
+case $key in
+start)
+echo "start the service"
+;;
+stop)
+echo "stop the service"
+;;
+restart)
+echo "restart the service"
+;;
+*)
+echo "option not valid,choose start/stop/restart wisely"
+;;
+esac
